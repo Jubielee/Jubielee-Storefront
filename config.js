d1403ed474541b7b8e265e2b761f8799535e8c8f
Jubielee - Your Community Connecting App <77489878+Jubielee@users.noreply.github.com>
2026-08-15 08:46:14 -0400

    Load JubieStore coupon checkout module

--- config.js
+++ config.js
@@ -4,3 +4,10 @@ window.JUBIELEE_STOREFRONT_CONFIG = {
   supportUrl: "https://pwa.jubielee.com/",
   productsPerPage: 24
 };
+
+(function () {
+  var script = document.createElement("script");
+  script.src = "coupon.js?v=20260815-prize1";
+  script.async = true;
+  document.head.appendChild(script);
+})();
